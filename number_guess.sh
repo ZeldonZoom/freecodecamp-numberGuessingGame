11d9f4dc62b653482519316f025032dc4f5d0e47
Arnav Vashishth <zeldon.zoom@gmail.com>
2024-11-21 04:40:43 +0000

    fix: fixed somethin

--- number_guess.sh
+++ number_guess.sh
@@ -2,24 +2,25 @@
 
 PSQL="psql --username=postgres --dbname=number_guess -t --no-align -c"
 
-COUNT=0
-RANDOM_NUMBER=$(( $RANDOM % 1000 + 1 ))
+NUMBER_OF_GUESSES=0
+SECRET_NUMBER=$(( $RANDOM % 1000 + 1 ))
 echo $RANDOM_NUMBER
 
 echo "Enter your username:"
 read USERNAME
 
-GET_USER=$($PSQL "select * from users where username='$USERNAME';")
+GET_USER=$($PSQL "select username from users where username='$USERNAME';")
 # echo $GET_USER
 if [[ -z $GET_USER ]]
 then
   echo "Welcome, $USERNAME! It looks like this is your first time here."
   ADD_USER=$($PSQL "insert into users(username) values('$USERNAME')")
 else
-  IFS='|' read -r USER_ID USER <<< "$GET_USER"
+  
   # echo $USER_ID $USER
-  GET_BEST_GUESS=$($PSQL "select guesses from users left join games on users.user_id=games.user_id where username='$GET_USER' order by guesses limit 1;")
-  echo "Welcome back, $USER! You have played games, and your best game took $GET_BEST_GUESS guesses."
+  BEST_GAME=$($PSQL "select guesses from users left join games on users.user_id=games.user_id where username='$GET_USER' order by guesses limit 1;")
+  GAMES_PLAYED=$($PSQL "select count(guesses) from users left join games on users.user_id=games.user_id where username='arnav';")
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
 while true
@@ -30,15 +31,16 @@ do
   if ! [[ $INPUT =~ ^-?[0-9]+$ ]]; then
     echo "That is not an integer, guess again:"
   else
-    COUNT=$(($COUNT + 1))
-    echo $COUNT
-    if [ $INPUT -gt $RANDOM_NUMBER ]; then
+    NUMBER_OF_GUESSES=$(($NUMBER_OF_GUESSES + 1))
+    # echo $COUNT
+    if [ $INPUT -gt $SECRET_NUMBER ]; then
       echo "It's lower than that, guess again:"
-    elif [ $INPUT -lt $RANDOM_NUMBER ]; then
-      echo "It's greater than that, guess again:"
+    elif [ $INPUT -lt $SECRET_NUMBER ]; then
+      echo "It's higher than that, guess again:"
     else
-      echo "You guessed it in $COUNT tries. The secret number was $RANDOM_NUMBER. Nice job!"
-      ADD_GAME=$($PSQL "insert into games(user_id, guesses) values($USER_ID, $COUNT)")
+      echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+      GET_ID=$($PSQL "select user_id from users where username='$USERNAME';")
+      ADD_GAME=$($PSQL "insert into games(user_id, guesses) values($GET_ID, $NUMBER_OF_GUESSES)")
       break
     fi
   fi
